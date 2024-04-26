34574551c9bfa1440b9a2051c1a9d421c2d94680
Ikmal Ali Azhari <100822093+ikmalalo@users.noreply.github.com>
2024-04-26 13:18:48 +0800

    Update connector.py

--- testcapstone/Model/connector.py
+++ testcapstone/Model/connector.py
@@ -1,11 +1,11 @@
 import mysql.connector
 from mysql.connector import Error
 
-hostname = "izh.h.filess.io"
-database = "ReyTop_unlessblow"
+hostname = "k76.h.filess.io"
+database = "Reytop2_stopheldto"
 port = "3306"
-username = "ReyTop_unlessblow"
-password = "234f1036bad6ab5930d6d3a10a4868b88ebe3e18"
+username = "Reytop2_stopheldto"
+password = "b9cd2a044f066ba96021d5b0e31b0ebd92f6c713"
 
 try:
     connection = mysql.connector.connect(host=hostname, database=database, user=username, password=password, port=port)
